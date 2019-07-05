c9a08a58ecdaae992996091c8f22ffb4fce0b649
Megan Sullivan <msulliv@thoughtworks.com>
2019-07-05 01:20:13 -0700

    Restructure generators to have a single export, and update outline for main

---
+++ generators/index.js
@@ -0,0 +1,5 @@
+import { generate_LD_r_from_rPrime } from './8-bit-transfer-and-io/LD_r_from_rPrime';
+
+export default [
+  generate_LD_r_from_rPrime
+];
--- main.js
+++ main.js
@@ -7,6 +7,14 @@
 // set first row to ['', 'x0', ... , 'xF']
 // set first column to ['', '0x', ... , 'Fx']
 
-// call each generator function, and iterate over opcodes to place objects into opCodesGrid
+// call each generator function
+const generateAllInstructions = () => {
+  
+  return instructions; // list containing all instruction objects
+}
+
+// iterate over opcodes to place objects into opCodesGrid
 
 // use opCodesGrid to generate an HTML table
+
+
