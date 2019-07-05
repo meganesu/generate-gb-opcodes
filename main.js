74840236f0f7eea4f69a02411253c026c0e2a0a3
Megan Sullivan <msulliv@thoughtworks.com>
2019-07-05 00:54:36 -0700

    Outline structure of main method

---
+++ main.js
@@ -0,0 +1,12 @@
+// import generator functions
+
+// Set up:
+
+// opCodesGrid: [16][16]
+
+// set first row to ['', 'x0', ... , 'xF']
+// set first column to ['', '0x', ... , 'Fx']
+
+// call each generator function, and iterate over opcodes to place objects into opCodesGrid
+
+// use opCodesGrid to generate an HTML table
