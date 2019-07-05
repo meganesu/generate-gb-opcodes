d889314100185ef93cf7764d9bf0f0aed0b93c69
Megan Sullivan <msulliv@thoughtworks.com>
2019-07-05 16:01:02 -0700

    Add LD r <- n8 to generators index

--- src/generators/index.js
+++ src/generators/index.js
@@ -1,5 +1,7 @@
 import { generate_LD_r_from_rPrime } from './8-bit-transfer-and-io/LD_r_from_rPrime';
+import { generate_LD_r_from_n8 } from './8-bit-transfer-and-io/LD_r_from_n';
 
 export default [
-  generate_LD_r_from_rPrime
+  generate_LD_r_from_rPrime,
+  generate_LD_r_from_n8
 ];
