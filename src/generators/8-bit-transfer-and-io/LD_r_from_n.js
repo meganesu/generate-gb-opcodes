fa45dff295d12fd423344c5908afb4af69e09b5e
Megan Sullivan <msulliv@thoughtworks.com>
2019-07-05 15:59:27 -0700

    Add LD r, n (LD r <- n) and update binary to hex converter to pad with 0

--- src/generators/8-bit-transfer-and-io/LD_r_from_n.js
+++ src/generators/8-bit-transfer-and-io/LD_r_from_n.js
@@ -1,3 +1,5 @@
+import { convertBinaryStringToHexString } from '../../helpers/convert-binary-string-to-hex-string';
+
 const registerBinaryCodes = {
   A: '111',
   B: '000',
@@ -8,3 +10,27 @@ const registerBinaryCodes = {
   L: '101'
 }
 
+// LD r, n
+export const generate_LD_r_from_n8 = () => {
+  //  0  0 r2 r1 r0  1  1  0
+  // n7 n6 n5 n4 n3 n2 n1 n0
+  const instructions = [];
+
+  for (let register of Object.keys(registerBinaryCodes)) {
+    const instruction = {};
+
+    instruction.mnemonic = `LD ${register}, n8`;
+    instruction.flags = {};
+    instruction.cycles = 2;
+
+    const opCodeInBinary = `00${registerBinaryCodes[register]}110`
+    instruction.opCode = convertBinaryStringToHexString(opCodeInBinary);
+
+    instructions.push(instruction);
+  }
+
+  console.log(instructions);
+  return instructions;
+}
+
+generate_LD_r_from_n8();
--- src/helpers/convert-binary-string-to-hex-string.js
+++ src/helpers/convert-binary-string-to-hex-string.js
@@ -1,5 +1,5 @@
 export const convertBinaryStringToHexString = (binaryString) => {
   const decimalValue = parseInt(binaryString, 2);
-  const hexString = decimalValue.toString(16);
+  const hexString = decimalValue.toString(16).padStart(2, '0');
   return hexString.toUpperCase();
 };
