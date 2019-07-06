4c2478aa824c935421c1bf2a3cc155115b676c06
Megan Sullivan <msulliv@thoughtworks.com>
2019-07-05 20:32:21 -0700

    Add LD (HL) <- r

--- src/generators/index.js
+++ src/generators/index.js
@@ -1,9 +1,11 @@
 import { generate_LD_r_from_rPrime } from './8-bit-transfer-and-io/LD_r_from_rPrime';
 import { generate_LD_r_from_n8 } from './8-bit-transfer-and-io/LD_r_from_n';
 import { generate_LD_r_from_value_at_HL } from './8-bit-transfer-and-io/LD_r_from_value_at_HL';
+import { generate_LD_memory_at_HL_from_r } from './8-bit-transfer-and-io/LD_memory_at_HL_from_r';
 
 export default [
   generate_LD_r_from_rPrime,
   generate_LD_r_from_n8,
-  generate_LD_r_from_value_at_HL
+  generate_LD_r_from_value_at_HL,
+  generate_LD_memory_at_HL_from_r
 ];
