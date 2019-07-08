48727faecf1520be1f96160319abae5ea5bf9d80
Megan Sullivan <msulliv@thoughtworks.com>
2019-07-08 09:13:44 -0700

    Add LD (HL) <- n8

---
+++ src/generators/8-bit-transfer-and-io/LD_memory_at_HL_from_n.js
@@ -0,0 +1,12 @@
+import { convertBinaryStringToHexString } from "../../helpers/convert-binary-string-to-hex-string";
+
+export const generate_LD_memory_at_HL_from_n = () => {
+  const instruction = {};
+  instruction.mnemonic = "LD (HL), n8";
+  instruction.flags = {};
+  instruction.cycles = 3;
+
+  const opCodeInBinary = "00110110";
+  instruction.opCode = convertBinaryStringToHexString(opCodeInBinary);
+  return [instruction];
+};
--- src/generators/index.js
+++ src/generators/index.js
@@ -2,10 +2,12 @@ import { generate_LD_r_from_rPrime } from './8-bit-transfer-and-io/LD_r_from_rPr
 import { generate_LD_r_from_n8 } from './8-bit-transfer-and-io/LD_r_from_n';
 import { generate_LD_r_from_value_at_HL } from './8-bit-transfer-and-io/LD_r_from_value_at_HL';
 import { generate_LD_memory_at_HL_from_r } from './8-bit-transfer-and-io/LD_memory_at_HL_from_r';
+import { generate_LD_memory_at_HL_from_n } from './8-bit-transfer-and-io/LD_memory_at_HL_from_n';
 
 export default [
   generate_LD_r_from_rPrime,
   generate_LD_r_from_n8,
   generate_LD_r_from_value_at_HL,
-  generate_LD_memory_at_HL_from_r
+  generate_LD_memory_at_HL_from_r,
+  generate_LD_memory_at_HL_from_n
 ];
