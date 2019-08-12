5f5eb8c92e450ab09b90b7f50279264191eb54fa
Megan Sullivan <meganesulli@gmail.com>
2019-08-11 17:53:47 -0700

    Extract op-code-grid into its own component

---
+++ src/components/op-code-table/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import instructionGenerators from '../../generators';
+import { createInitialOpCodesGrid, setCellForOpCode, buildGridString } from '../../helpers/grid-helpers';
+
+// Set up:
+const opCodesGrid = createInitialOpCodesGrid(); // opCodesGrid: Array[17][17]
+
+// call each generator function
+const generateAllInstructions = () => {
+  let instructions = [];
+
+  for (let generatorFunction of instructionGenerators) {
+    instructions = instructions.concat(generatorFunction());
+  }
+  
+  return instructions; // list containing all instruction objects
+}
+
+const instructions = generateAllInstructions();
+
+// iterate over opcodes to place objects into opCodesGrid
+instructions.forEach(instruction => {
+  setCellForOpCode(instruction.opCode, instruction, opCodesGrid);
+});
+
+const OpCodeTable = () => {
+  const text = buildGridString(opCodesGrid);
+
+  return (
+    <div id="op-code-table" dangerouslySetInnerHTML={{__html: text}} />
+  );
+};
+
+export default OpCodeTable;
--- src/main.js
+++ src/main.js
@@ -1,34 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import instructionGenerators from './generators';
-import { createInitialOpCodesGrid, setCellForOpCode, buildGridString } from './helpers/grid-helpers';
-
-// Set up:
-const opCodesGrid = createInitialOpCodesGrid(); // opCodesGrid: Array[17][17]
-
-// call each generator function
-const generateAllInstructions = () => {
-  let instructions = [];
-
-  for (let generatorFunction of instructionGenerators) {
-    instructions = instructions.concat(generatorFunction());
-  }
-  
-  return instructions; // list containing all instruction objects
-}
-
-const instructions = generateAllInstructions();
-
-// iterate over opcodes to place objects into opCodesGrid
-instructions.forEach(instruction => {
-  setCellForOpCode(instruction.opCode, instruction, opCodesGrid);
-});
+import OpCodeTable from './components/op-code-table';
 
 const App = ({text}) => (
-  <div id="op-code-table" dangerouslySetInnerHTML={{__html: text}} />
-)
-// use opCodesGrid to generate an HTML table
-ReactDOM.render(
-  <App text={buildGridString(opCodesGrid)} />,
-  document.getElementById('container')
+  <OpCodeTable />
 );
+
+// use opCodesGrid to generate an HTML table
+ReactDOM.render(<App />, document.getElementById('container'));
