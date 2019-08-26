712f80c381b0c901072af9c919713bdd69ad09f6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-26 16:45:23 +0000

    Deploy meganesu/generate-gb-op-codes to github.com/meganesu/generate-gb-op-codes.git:gh-pages

--- webpack.common.js
+++ webpack.common.js
@@ -3,14 +3,14 @@ const path = require('path');
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
   },
   module: {
     rules: [
       {
         test: /\.js/,
-        exclude: /(node_modules|build)/,
+        exclude: /(node_modules|dist)/,
         use: {
           loader: 'babel-loader'
         }
