6ce6dbbf6dd6906d24615113abcccad66551a24d
Megan Sullivan <msulliv@thoughtworks.com>
2019-08-27 09:46:01 -0700

    Add ADD A, d8

---
+++ src/generators/8-bit-arithmetic-and-logical-operation/ADD_A_d8.js
@@ -0,0 +1,19 @@
+import { convertBinaryStringToHexString } from "../../helpers/convert-binary-string-to-hex-string";
+
+const generate_ADD_A_d8 = () => {
+  const instruction = {};
+
+  instruction.mnemonic = 'ADD A, d8';
+  instruction.flags = {
+    CY: '8-bit',
+    H: '8-bit',
+    N: '0',
+    Z: 'Z'
+  };
+  instruction.cycles = 2;
+
+  const opCodeInBinary = '11000110';
+  instruction.opCode = convertBinaryStringToHexString(opCodeInBinary);
+
+  return instruction;
+}
