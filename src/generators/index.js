ecefd5494defaf475aa35acfd8ace25b901410f8
Megan Sullivan <meganesulli@gmail.com>
2019-09-02 16:53:22 -0700

    Add ADD A, (HL)

--- src/generators/index.js
+++ src/generators/index.js
@@ -30,6 +30,7 @@ import { generate_LD_memory_at_a16_from_SP } from './16-bit-transfer/LD_memory_a
 // 8-bit arithmetic and logical operation instructions
 import { generate_ADD_A_r8 } from './8-bit-arithmetic-and-logical-operation/ADD_A_r8';
 import { generate_ADD_A_d8 } from './8-bit-arithmetic-and-logical-operation/ADD_A_d8';
+import { generate_ADD_A_memory_at_HL } from './8-bit-arithmetic-and-logical-operation';
 
 export const instructionGenerators = [
   generate_LD_r_from_rPrime,
@@ -58,7 +59,8 @@ export const instructionGenerators = [
   generate_HL_from_SP_plus_s8,
   generate_LD_memory_at_a16_from_SP,
   generate_ADD_A_r8,
-  generate_ADD_A_d8
+  generate_ADD_A_d8,
+  generate_ADD_A_memory_at_HL
 ];
 
 // call each generator function
