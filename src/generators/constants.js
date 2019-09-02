d26478c7dfd5a0c035e632203df81a7d26139193
Megan Sullivan <meganesulli@gmail.com>
2019-09-02 00:14:10 -0700

    Complete list of constants for instruction types

--- src/generators/constants.js
+++ src/generators/constants.js
@@ -1,5 +1,11 @@
 export const instructionTypes = {
   EIGHT_BIT_TRANSFER_AND_IO: 'eight-bit-transfer-and-io',
+  SIXTEEN_BIT_TRANSFER: 'sixteen-bit-transfer',
   EIGHT_BIT_ARITHMETIC_AND_LOGICAL_OPERATION: 'eight-bit-arithmetic-and-logical-operation',
-  SIXTEEN_BIT_TRANSFER: 'sixteen-bit-transfer'
-}
+  SIXTEEN_BIT_ARITHMETIC_OPERATION: 'sixteen-bit-arithmetic-operation',
+  ROTATE_SHIFT: 'rotate-shift',
+  BIT_OPERATION: 'bit-operation',
+  JUMP: 'jump',
+  CALL_AND_RETURN: 'call-and-return',
+  GENERAL_PURPOSE_ARITHMETIC_OPERATIONS_AND_CPU_CONTROL: 'general-purpose-arithmetic-operations-and-cpu-control',
+};
