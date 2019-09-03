c505482035901a6d70787006da1a84ebd63177ad
Megan Sullivan <meganesulli@gmail.com>
2019-09-02 22:06:36 -0700

    Add SBC A, (HL)

---
+++ src/generators/8-bit-arithmetic-and-logical-operation/SBC_A_memory_at_HL.js
@@ -0,0 +1,21 @@
+import { instructionTypes } from '../constants';
+import { convertBinaryStringToHexString } from '../../helpers/convert-binary-string-to-hex-string';
+
+export const generate_SBC_A_memory_at_HL = () => {
+  const instruction = {};
+
+  instruction.mnemonic = 'SBC A, (HL)';
+  instruction.type = instructionTypes.EIGHT_BIT_ARITHMETIC_AND_LOGICAL_OPERATION;
+  instruction.flags = {
+    CY: '8-bit',
+    H: '8-bit',
+    N: '1',
+    Z: 'Z'
+  };
+  instruction.cycles = 2;
+
+  const opCodeInBinary = '10011110';
+  instruction.opCode = convertBinaryStringToHexString(opCodeInBinary);
+
+  return instruction;
+};
