366a1ee64b24616331ec6ddcbb63f739e0194915
Megan Sullivan <meganesulli@gmail.com>
2019-09-02 22:30:24 -0700

    Add AND d8

---
+++ src/generators/8-bit-arithmetic-and-logical-operation/AND_d8.js
@@ -0,0 +1,21 @@
+import { instructionTypes } from '../constants';
+import { convertBinaryStringToHexString } from '../../helpers/convert-binary-string-to-hex-string';
+
+export const generate_AND_d8 = () => {
+  const instruction = {};
+
+  instruction.mnemonic = 'AND d8';
+  instruction.type = instructionTypes.EIGHT_BIT_ARITHMETIC_AND_LOGICAL_OPERATION;
+  instruction.flags = {
+    CY: '0',
+    H: '1',
+    N: '0',
+    Z: 'Z'
+  };
+  instruction.cycles = 2;
+
+  const opCodeInBinary = '11100110';
+  instruction.opCode = convertBinaryStringToHexString(opCodeInBinary);
+
+  return instruction;
+};
