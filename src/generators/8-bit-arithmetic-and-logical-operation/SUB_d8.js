5af455e412376ca829ac5699b355f15fbdb86621
Megan Sullivan <meganesulli@gmail.com>
2019-09-02 21:26:56 -0700

    Add SUB d8

---
+++ src/generators/8-bit-arithmetic-and-logical-operation/SUB_d8.js
@@ -0,0 +1,21 @@
+import { instructionTypes } from '../constants';
+import { convertBinaryStringToHexString } from '../../helpers/convert-binary-string-to-hex-string';
+
+export const generate_SUB_d8 = () => {
+  const instruction = {};
+
+  instruction.mnemonic = 'SUB n8';
+  instruction.type = instructionTypes.EIGHT_BIT_ARITHMETIC_AND_LOGICAL_OPERATION;
+  instruction.flags = {
+    CY: '8-bit',
+    H: '8-bit',
+    N: '1',
+    Z: 'Z'
+  };
+  instruction.cycles = 2;
+
+  const opCodeInBinary = '11010110';
+  instruction.opCode = convertBinaryStringToHexString(opCodeInBinary);
+
+  return instruction;
+};
