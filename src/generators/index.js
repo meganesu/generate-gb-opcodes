d31f0abfa84df09925e719d82d378b897d6ee752
Megan Sullivan <meganesulli@gmail.com>
2019-09-28 17:04:02 -0700

    Add RR r8

--- src/generators/index.js
+++ src/generators/index.js
@@ -74,6 +74,7 @@ import { generate_RL_r8 } from './rotate-shift/RL_r8';
 import { generate_RL_memory_at_HL } from './rotate-shift/RL_memory_at_HL';
 import { generate_RRC_r8 } from './rotate-shift/RRC_r8';
 import { generate_RRC_memory_at_HL } from './rotate-shift/RRC_memory_at_HL';
+import { generate_RR_r8 } from './rotate-shift/RR_r8';
 
 export const instructionGenerators = [
   generate_LD_r_from_rPrime,
@@ -142,7 +143,8 @@ export const instructionGenerators = [
   generate_RL_r8,
   generate_RL_memory_at_HL,
   generate_RRC_r8,
-  generate_RRC_memory_at_HL
+  generate_RRC_memory_at_HL,
+  generate_RR_r8
 ];
 
 // call each generator function
