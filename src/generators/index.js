e359be05c4a92f5db91606367c593807c69a6359
Megan Sullivan <meganesulli@gmail.com>
2019-09-28 17:19:21 -0700

    Add SRA (HL)

--- src/generators/index.js
+++ src/generators/index.js
@@ -79,6 +79,7 @@ import { generate_RR_memory_at_HL } from './rotate-shift/RR_memory_at_HL';
 import { generate_SLA_r8 } from './rotate-shift/SLA_r8';
 import { generate_SLA_memory_at_HL } from './rotate-shift/SLA_memory_at_HL';
 import { generate_SRA_r8 } from './rotate-shift/SRA_r8';
+import { generate_SRA_memory_at_HL } from './rotate-shift/SRA_memory_at_HL';
 
 export const instructionGenerators = [
   generate_LD_r_from_rPrime,
@@ -152,7 +153,8 @@ export const instructionGenerators = [
   generate_RR_memory_at_HL,
   generate_SLA_r8,
   generate_SLA_memory_at_HL,
-  generate_SRA_r8
+  generate_SRA_r8,
+  generate_SRA_memory_at_HL
 ];
 
 // call each generator function
