1be5d788af98fd9d3286d13fb030951db98f8d00
Megan Sullivan <meganesulli@gmail.com>
2019-10-14 09:21:10 -0700

    Add SET b1, (HL)

---
+++ src/generators/bit-operation/SET_b1_memory_at_HL.js
@@ -0,0 +1,23 @@
+import { instructionTypes } from "../constants";
+import { bitBinaryValues } from './constants';
+import { convertBinaryStringToHexString } from "../../helpers/convert-binary-string-to-hex-string";
+
+export const generate_SET_b1_memory_at_HL = () => {
+  const instructions = [];
+
+  for (let bit in bitBinaryValues) {
+    const instruction = {};
+
+    instruction.mnemonic = `SET ${bit}, (HL)`;
+    instruction.type = instructionTypes.BIT_OPERATION;
+    instruction.flags = {}
+    instruction.cycles = 4;
+
+    const opCodeInBinary = `1100101111${bitBinaryValues[bit]}110`;
+    instruction.opCode = convertBinaryStringToHexString(opCodeInBinary);
+
+    instructions.push(instruction);
+  }
+
+  return instructions;
+}
