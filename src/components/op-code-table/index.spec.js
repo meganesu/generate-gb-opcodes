1196b5d657fb8f8d60817e6860bc964fe490fc61
Megan Sullivan <meganesulli@gmail.com>
2019-10-24 12:47:36 -0700

    Refactor OpCodeTable to use InstructionCell component

--- src/components/op-code-table/index.spec.js
+++ src/components/op-code-table/index.spec.js
@@ -1,9 +1,30 @@
 import OpCodeTable from '.';
+import InstructionCell from '../instruction-cell';
 import { mount } from 'enzyme';
 
 describe('OpCodeTable', () => {
   let component;
   let grid;
+  const instructions = [
+    {
+      mnemonic: 'TEST1',
+      type: 'test-type',
+      flags: {},
+      cycles: '1',
+    },
+    {
+      mnemonic: 'TEST2',
+      type: 'test-type',
+      flags: {},
+      cycles: '1',
+    },
+    {
+      mnemonic: 'TEST3',
+      type: 'test-type',
+      flags: {},
+      cycles: '1',
+    },
+  ]
 
   beforeAll(() => {
     grid = new Array(16);
@@ -13,6 +34,10 @@ describe('OpCodeTable', () => {
         grid[i][j] = '';
       }
     }
+
+    grid[3][8] = instructions[0];
+    grid[4][5] = instructions[1];
+    grid[6][8] = instructions[2];
 
     component = mount(<OpCodeTable opCodesGrid={grid} />);
   });
@@ -26,4 +51,7 @@ describe('OpCodeTable', () => {
   it('renders the right number of cells in the table', () => {
     expect(component.find('td.instruction')).toHaveLength(16*16);
   });
+  it('renders the right number of InstructionCell components', () => {
+    expect(component.find(InstructionCell)).toHaveLength(instructions.length);
+  })
 });
