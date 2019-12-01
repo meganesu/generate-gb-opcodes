fc39366091df889566553443eeeb612f7b427636
Megan Sullivan <meganesulli@gmail.com>
2019-11-30 22:16:30 -0800

    feat: update sidebar structure and styles - use flex instead of float - wrap instruction mnemonic and close sidebar button

--- src/components/details-sidebar/index.js
+++ src/components/details-sidebar/index.js
@@ -1,13 +1,23 @@
 const DetailsSidebar = ({activeInstruction, hideSidebar}) => (
   <div id="details-sidebar">
-    {activeInstruction.mnemonic}
-    <button
-      id="close-sidebar"
-      aria-label="close details sidebar"
-      onClick={hideSidebar}
-    >
-      X
-    </button>
+    <div id="sidebar-header">
+      <h3>
+        {activeInstruction.mnemonic}
+      </h3>
+      <button
+        id="close-sidebar"
+        aria-label="close details sidebar"
+        onClick={hideSidebar}
+      >
+        X
+      </button>
+    </div>
+    <section>
+      <ul>
+        <li>Opcode: 0x{activeInstruction.opCode}</li>
+        <li>Description: Coming Soon</li>
+      </ul>
+    </section>
   </div>
 );
 
